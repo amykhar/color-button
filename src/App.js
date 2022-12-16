ce4ef42363c070ab8d01768b20d4823ca50549ae
Amy Anuszewski <amykhar@gmail.com>
2022-12-16 07:46:56 -0500

    Installed prettier and set it up in phpstorm

--- src/App.js
+++ src/App.js
@@ -1,67 +1,69 @@
-import './App.css';
-import {useState} from 'react';
-import ColorButton from './components/ColorButton';
-import Checkbox from './components/Checkbox';
+import "./App.css";
+import { useState } from "react";
+import ColorButton from "./components/ColorButton";
+import Checkbox from "./components/Checkbox";
 
-const red = 'MediumVioletRed';
-const blue = 'MidnightBlue';
-const gray = 'gray';
-const checkboxLabel = 'Disable button';
+const red = "MediumVioletRed";
+const blue = "MidnightBlue";
+const gray = "gray";
+const checkboxLabel = "Disable button";
 
 export function replaceCamelWithSpaces(colorName) {
-    return colorName.replace(/\B([A-Z])\B/g, ' $1');
+  return colorName.replace(/\B([A-Z])\B/g, " $1");
 }
 
 export function replaceSpacesWithCamel(colorName) {
-    colorName = colorName.replace('Change to ', '');
-    colorName = colorName.replace(/\s/g, '');
-    return colorName;
+  colorName = colorName.replace("Change to ", "");
+  colorName = colorName.replace(/\s/g, "");
+  return colorName;
 }
 
 function App() {
-    const [buttonColor, setButtonColor] = useState(red);
-    const [buttonText, setButtonText] = useState('Change to ' + replaceCamelWithSpaces(blue));
-    const [checked, setChecked] = useState(false);
-    const [disabled, setDisabled] = useState(false);
+  const [buttonColor, setButtonColor] = useState(red);
+  const [buttonText, setButtonText] = useState(
+    "Change to " + replaceCamelWithSpaces(blue)
+  );
+  const [checked, setChecked] = useState(false);
+  const [disabled, setDisabled] = useState(false);
 
-    const clickHandler = () => {
-        const redLabel = 'Change to ' + replaceCamelWithSpaces(red);
-        const blueLabel = 'Change to ' + replaceCamelWithSpaces(blue);
+  const clickHandler = () => {
+    const redLabel = "Change to " + replaceCamelWithSpaces(red);
+    const blueLabel = "Change to " + replaceCamelWithSpaces(blue);
 
-        const newButtonColor = buttonColor === red ? blue : red;
-        const newButtonText = buttonText === redLabel ? blueLabel : redLabel;
-        setButtonColor(newButtonColor);
-        setButtonText(newButtonText);
+    const newButtonColor = buttonColor === red ? blue : red;
+    const newButtonText = buttonText === redLabel ? blueLabel : redLabel;
+    setButtonColor(newButtonColor);
+    setButtonText(newButtonText);
+  };
+
+  const checkboxClickHandler = () => {
+    const newChecked = !checked;
+    const newDisabled = !disabled;
+    setChecked(newChecked);
+    setDisabled(newDisabled);
+    if (newDisabled) {
+      setButtonColor(gray);
+    } else {
+      console.log(buttonText);
+      setButtonColor(replaceSpacesWithCamel(buttonText));
     }
+  };
 
-    const checkboxClickHandler = () => {
-        const newChecked = !checked;
-        const newDisabled = !disabled;
-        setChecked(newChecked);
-        setDisabled(newDisabled);
-        if (newDisabled) {
-            setButtonColor(gray);
-        } else {
-            console.log(buttonText);
-            setButtonColor(replaceSpacesWithCamel(buttonText));
-        }
-    }
-
-    return (
-        <div>
-            <ColorButton
-                clickHandler={clickHandler}
-                disabled={disabled}
-                buttonColor={buttonColor}
-                buttonText={buttonText}
-            />
-            <Checkbox
-                checked={checked}
-                labelText={checkboxLabel}
-                onChange={checkboxClickHandler}
-            />
-        </div>
-    );
+  return (
+    <div>
+      <ColorButton
+        clickHandler={clickHandler}
+        disabled={disabled}
+        buttonColor={buttonColor}
+        buttonText={buttonText}
+      />
+      <Checkbox
+        checked={checked}
+        labelText={checkboxLabel}
+        onChange={checkboxClickHandler}
+      />
+    </div>
+  );
 }
 
 export default App;
